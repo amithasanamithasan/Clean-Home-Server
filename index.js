21bb390157a1ce84f2f2d93d658dbd6cf0d82c69
amithasanamithasan <amit35-1773@diu.edu.bd>
2024-08-20 20:03:23 +0600

    created user cartcollection and user post carts reqbody

--- index.js
+++ index.js
@@ -25,12 +25,39 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const serviceCollection= client.db("HomeCleanDb").collection("service");
+    const ratingCollection= client.db("HomeCleanDb").collection("rating");
+    const cartCollection= client.db("HomeCleanDb").collection("carts");
+// services for get all database 
+    app.get("/service", async(req,res)=>{
+        const result = await serviceCollection.find().toArray();
+        res.send(result);
+
+    });
+    // rating get all client for database
+    app.get("/rating", async(req,res)=>{
+
+      const result =await ratingCollection.find().toArray();
+      res.send(result);
+
+    });
+    
+    // carts coleection 
+    app.post('carts' ,async(req,res)=>{
+      const cartItem=req.body;
+      const result =await cartCollection.insertOne(cartItem);
+      res.send(result);
+
+    });
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
@@ -43,3 +70,9 @@ app.get('/',(req, res)=>{
 app.listen(port, ()=>{
     console.log(`Home Cleaning servicess avilable ${port}`);
 });
+// NAMING CONVENITTION
+// app.get("/users")shob users ke pite chile 
+// app.get("/users/:id")spaceifiq user pate chie 
+// app.post("/users") akta users created make database
+// app.put("/users") akta spaceifiq users ke kichu korte chile
+// app.delete("/users") akta spaceifiq users ke delete korte chile
