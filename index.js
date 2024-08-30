d4313d6db9236f7af42f381e0d4e6345a9e2089a
amithasanamithasan <amit35-1773@diu.edu.bd>
2024-08-30 21:09:30 +0600

    get users Load all users on the Admin Dashboard page

--- index.js
+++ index.js
@@ -77,7 +77,13 @@ app.post('/users',async (req,res)=>{
   const result= await userCollection.insertOne(user);
   res.send(result);
 })
+// Admin dashboard  signin all users show in the table 
+app.get('/users',async (req,res)=>{
 
+  const result= await userCollection.find().toArray();
+  res.send(result);
+
+})
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
